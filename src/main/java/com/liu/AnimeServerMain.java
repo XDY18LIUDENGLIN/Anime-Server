18a925a8332eeaae05044dc13dc340ed7b841509
LIUDENGLIN <1091669791@QQ.com>
2022-07-10 13:55:29 +0800

    init: 项目文件初始化

---
+++ src/main/java/com/liu/AnimeServerMain.java
@@ -0,0 +1,16 @@
+package com.liu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : LIUDENGLIN
+ * @Email : 2092669791@qq.com
+ * @date : 2022/7/10 13:44
+ */
+@SpringBootApplication
+public class AnimeServerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeServerMain.class, args);
+    }
+}
---
+++ src/main/java/com/liu/config/RedisConfig.java
@@ -0,0 +1,9 @@
+package com.liu.config;
+
+/**
+ * @author : LIUDENGLIN
+ * @Email : 2092669791@qq.com
+ * @date : 2022/7/10 13:47
+ */
+public class RedisConfig {
+}
